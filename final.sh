6c652ded7f44a938cdda759e44ab783cb8fe13c7
Hitman <38419085+hitman-045@users.noreply.github.com>
2021-08-24 00:01:53 +0530

    Update final.sh

--- final.sh
+++ final.sh
@@ -4,7 +4,7 @@ wget https://raw.githubusercontent.com/hitman-045/tomcatinstall/master/java.sh
 
 chmod +x java.sh
 
-./java.sh
+sh java.sh
 source /etc/profile
 sleep 1
 source /etc/profile
@@ -13,4 +13,4 @@ echo $JAVA_HOME
 echo $JRE_HOME
 wget https://raw.githubusercontent.com/hitman-045/tomcatinstall/master/tomcat.sh
 chmod +x tomcat.sh
-./tomcat.sh
+sh tomcat.sh
