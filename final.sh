2974447d27b7f531ca384c0f0d0b5e94dba6fd2b
Hitman <38419085+hitman-045@users.noreply.github.com>
2021-08-23 22:59:43 +0530

    Update final.sh

--- final.sh
+++ final.sh
@@ -1,9 +1,12 @@
 # tomcatinstall-use this steps to install tomcat with all resources we need like tomcat-users.xml.
 
 #!bin/bash
-sudo -i;
-wget https://raw.githubusercontent.com/hitman-045/tomcatinstall/master/tomcat.sh;
-chmod +x tomcat.sh;
-sh tomcat.sh;
-sleep 4
-/etc/init.d/tomcatstart
+sudo -i
+wget https://raw.githubusercontent.com/hitman-045/jenkinsjava/master/java.sh 
+chmod +x java.sh
+sh /java.sh
+source /etc/profile
+
+wget https://raw.githubusercontent.com/hitman-045/tomcatinstall/master/tomcat.sh
+chmod +x tomcat.sh
+sh tomcat.sh
